7dd678eb0b177818d0f974a88b67f7bba0f07d21
francollovera <107972925+francollovera@users.noreply.github.com>
2022-07-01 20:47:39 -0300

    Add files via upload

---
+++ js/reloj.js
@@ -0,0 +1,6 @@
+let mujer = document.getElementById('cambiar1');
+mujer.addEventListener('click', iramujer);
+
+function iramujer(){
+window.open('file:///C:/Users/franc/OneDrive/Desktop/Programacion1/pagina%20web%205/relojmujer.html')
+};
